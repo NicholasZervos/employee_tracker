694fce4ac3703ff30f6f6fb7d60d3a192fc9d6e1
NicholasZervos <nicholaszervos25@gmail.com>
2022-10-26 23:43:34 -0400

    seeds updated/fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,21 +2,21 @@ USE employee_tracker
 
 INSERT INTO departments (name)
 VALUES
-('Engineering', 2),
-('Finance', 3),
-('Legal', 4),
-('Sales', 1);
+('Engineering'),
+('Finance'),
+('Legal'),
+('Sales');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Sales Lead', 100000, 1),
-('Salesperson', 80000, 2),
-('Lead Engineer', 150000, 3), 
-('Software Engineer', 120000, 4),
-('Account Manager', 160000, 5),
-('Accountant', 125000, 6),
-('Legal Team Lead', 250000, 7),
-('Lawyer', 190000, 8);
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2), 
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
